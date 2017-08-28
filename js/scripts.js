983240ae5a201c8b9152bbb3f1b3f5995fe54ee4
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-08-28 17:35:14 +0200

    Zadanie 8.4

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+if (name === "Marta") {
+//	alert('Hello, Welcome back, ' + name);
+} else {
+/*	alert('Hello, First time here, ' + name);	*/
+} 
+console.log('Hello ' + name);
+// koniec skryptu
